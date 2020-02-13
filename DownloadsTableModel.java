5290e5db2bc4768d44087c84a9f7ec932caa15ed
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-02-14 02:20:24 +0530

    Downloads Table Model  all abstract methods written

---
+++ DownloadsTableModel.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package downloadmanager;
+
+import java.util.Observable;
+import java.util.Observer;
+import javax.swing.table.AbstractTableModel;
+
+/**
+ *
+ * @author USER
+ */
+public class DownloadsTableModel extends AbstractTableModel implements Observer{
+
+    @Override
+    public int getRowCount() {
+        
+    }
+
+    @Override
+    public int getColumnCount() {
+         }
+
+    @Override
+    public Object getValueAt(int rowIndex, int columnIndex) {
+        }
+
+    @Override
+    public void update(Observable o, Object arg) {
+        }
+    
+    
+}
