50743d04e36745af7e49132101fd7a5d80c41025
VIJAY GUPTA <50875814+VIjay-Nits@users.noreply.github.com>
2020-02-14 19:17:54 +0530

    GUI updated

--- DownloadManager.java
+++ DownloadManager.java
@@ -5,14 +5,12 @@
  */
 package downloadmanager;
 
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
-import java.util.Observable;
-import java.util.Observer;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JTable;
-import javax.swing.JTextField;
+import java.awt.BorderLayout;
+import java.awt.event.*;
+import java.util.*;
+import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
 
 /**
  *
@@ -31,16 +29,101 @@ public class DownloadManager extends JFrame implements Observer  {
     private JTable table;
     private JButton pauseButton,resumeButton,canelButton,clearButton;
     
-    private DownloadsTableModel tableModel1;//download table's data model
+    private DownloadsTableModel tableModel;//download table's data model
     private Download selectedDownload;
     
     private boolean clearing;//table selection is being cleared or not
     private void actionExit(){System.exit(0);}
+    
     public DownloadManager() {
         setTitle("Downlaod Manager");
         setSize(640,480);
-        addWindowListener(new WindowAdapter(){@Override
-        public void windowClosing(WindowEvent e){actionExit();}});
+        addWindowListener(new WindowAdapter(){
+            @Override
+            public void windowClosing(WindowEvent e){actionExit();}});
+        
+        JMenuBar menuBar=new JMenuBar();
+        JMenu fileMenu=new JMenu("File");
+        fileMenu.setMnemonic(KeyEvent.VK_F);
+        JMenuItem fileExitMenuItem=new JMenuItem("Exit",KeyEvent.VK_X);
+        fileExitMenuItem.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                actionExit();
+            }
+        });
+        fileMenu.add(fileExitMenuItem);
+        menuBar.add(menuBar);
+        
+        JPanel addPanel=new JPanel();
+        addTextField=new JTextField(30);
+        addPanel.add(addTextField);
+        JButton addButton =new JButton("Add Download");
+        addButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                actionAdd();
+            }
+        });
+        
+        addPanel.add(addButton);
+        tableModel=new DownloadsTableModel();
+        table=new JTable(tableModel);
+        table.getSelectionModel().addListSelectionListener(new ListSelectionListener(){
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                tableSelectionChanged();
+            }
+        });
+        //one row at a time
+        table.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        
+        ProgressRenderer renderer=new ProgressRenderer(0,100);
+        renderer.setStringPainted(true);
+        table.setDefaultRenderer(JProgressBar.class, renderer);
+        
+        table.setRowHeight((int)renderer.getPreferredSize().getHeight());
+        
+        JPanel downloadsPanel=new JPanel();
+        downloadsPanel.setBorder(
+                BorderFactory.createTitledBorder("Downloads"));
+        downloadsPanel.setLayout(new BorderLayout());
+        downloadsPanel.add(new JScrollPane(table),BorderLayout.CENTER);
+        
+        JPanel buttonsPanel=new JPanel();
+        pauseButton=new JButton("Pause");
+        pauseButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+                actionPause();
+            }
+        });
+        pauseButton.setEnabled(false);
+        buttonsPanel.add(pauseButton);
+        resumeButton=new JButton("Resume");
+        resumeButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                actionResume();
+            }
+        });
+        resumeButton.setEnabled(false);
+        buttonsPanel.add(resumeButton);
+        canelButton=new JButton("Cancel");
+        canelButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                actionClear();
+            }
+        });
+        clearButton.setEnabled(false);
+        buttonsPanel.add(clearButton);
+        
+        
+        
+        
+    
+    
     }
     
     
